b8331650435953ec12a092ca18cf73d5598762f0
victor17-code <2016110312@udh.edu.pe>
2024-10-12 06:37:38 -0500

    añadiendo dao a los modelos

--- src/main/java/com/olpasa/dto/ConductorDto.java
+++ src/main/java/com/olpasa/dto/ConductorDto.java
@@ -1,4 +1,23 @@
 package com.olpasa.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ConductorDto  {
+
+    private Integer codigo;
+    private Integer codigoEt;
+    private String licencia;
+    private String numeroDocumento;
+    private String categoria;
+    private String nombres;
+    private String paterno;
+    private String materno;
+    private String estado;
+    private String usuario;
+    private String codTipoDoc;
 }

3c1bcbf594af6ace1f783fd01b741ed3bb2f975b
victors1998 <veusebiosoto@gmail.com>
2025-10-24 16:52:34 -0500

    nhgt

---
+++ src/main/java/com/olpasa/controller/GuiaRemisionController.java
@@ -0,0 +1,14 @@
+package com.olpasa.controller;
+
+import com.olpasa.service.IGuiaRemisionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bancos")
+@RequiredArgsConstructor
+public class GuiaRemisionController {
+
+
+}

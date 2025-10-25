05cce9f424174736d64bd996059fe70b68653672
victors1998 <veusebiosoto@gmail.com>
2025-10-25 12:49:48 -0500

    nbh

--- src/main/java/com/olpasa/repo/IGuiaRemisionRepo.java
+++ src/main/java/com/olpasa/repo/IGuiaRemisionRepo.java
@@ -17,6 +17,6 @@ public interface IGuiaRemisionRepo extends IGenericoRepo<GuiaRemision, Integer> 
             "from guia_remision as gr \n" +
             "inner join persona as per on gr.codigo_destinatario=per.codigo\n" +
             "inner join detalle_guia as dg on gr.id_guia=dg.id_guia\n" +
-            "WHERE gr.facturado='NO'", nativeQuery = true)
+            "WHERE gr.facturado='NO' AND gr.estado='EMITIDO'", nativeQuery = true)
     List<GuiaRemisionDTO> findGuiaRemisionFacturado();
 }

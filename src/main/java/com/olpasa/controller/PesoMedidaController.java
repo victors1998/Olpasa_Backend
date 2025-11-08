841b95089d64ca2a52cce35775e06f01597e9fb6
victors1998 <veusebiosoto@gmail.com>
2025-11-08 12:52:37 -0500

    bhjhj

--- src/main/java/com/olpasa/controller/PesoMedidaController.java
+++ src/main/java/com/olpasa/controller/PesoMedidaController.java
@@ -1,10 +1,8 @@
 package com.olpasa.controller;
 
-import com.olpasa.dto.PMDto;
-import com.olpasa.dto.PesoDestare;
-import com.olpasa.dto.PesoMedidaDTO;
-import com.olpasa.dto.VDTO;
+import com.olpasa.dto.*;
 import com.olpasa.model.PesoMedida;
+import com.olpasa.model.Sector;
 import com.olpasa.model.Vehiculo;
 import com.olpasa.service.IPesoMedidaService;
 import com.olpasa.util.MapperUtil;
@@ -26,6 +24,13 @@ public class PesoMedidaController {
     private final IPesoMedidaService pesoMedidaService;
     private final MapperUtil mapperUtil;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<PesoMedidaDTO> findById(@PathVariable("id") Integer id) {
+        PesoMedida obj = pesoMedidaService.findById(id);
+        //return ResponseEntity.ok(convertToDto(obj));
+        return ResponseEntity.ok(mapperUtil.map(obj, PesoMedidaDTO.class));
+    }
+
     @GetMapping("/search/date")
     public ResponseEntity<List<PMDto>> buscarByFecha(@RequestParam(value = "date", defaultValue = "2024-08-01") String date) {
         LocalDate fecha = LocalDate.parse(date);
@@ -36,8 +41,15 @@ public class PesoMedidaController {
     @PostMapping
     public ResponseEntity<PesoMedidaDTO> save(@Valid @RequestBody PesoMedidaDTO dto) {
         PesoMedida obj = pesoMedidaService.save(mapperUtil.map(dto, PesoMedida.class));
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId_peso_medida()).toUri();
+        System.out.println("✅ Nueva ID registrada: " + obj.getId_peso_medida());
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId_peso_medida())
+                .toUri();
         return ResponseEntity.created(location).build();
     }
+
+
 
 }
